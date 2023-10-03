c0acdea7986e5d356bc795ee3ce56612b27cc721
Geraldi Sutanto <5717479+justomat@users.noreply.github.com>
2023-10-03 21:07:49 +0700

    fix eas

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,12 @@ import { ConfigContext } from 'expo/config'
 
 export default ({ config }: ConfigContext) => ({
   ...config,
+  owner: 'gsu',
+  slug: 'expo-chart-orderbook',
   extra: {
-    storybookEnabled: process.env.STORYBOOK_ENABLED
+    storybookEnabled: process.env.STORYBOOK_ENABLED,
+    eas: {
+      projectId: 'a90aa7f3-2978-43af-b8aa-50791ee20111'
+    }
   }
 })
